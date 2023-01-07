3f3aee4b4732edb30ca5dcdfbe9a9e9261ea55f1
ROMAN <shumeiko.roman92@gmail.com>
2023-01-07 13:53:24 +0200

    first

---
+++ js1/src/js/script.js
@@ -0,0 +1,17 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+
+// number = 6;
+// console.log(number);
+
+// const user = 'roman';
+
+// const arr = [0, 1, 2, 3];
+// console.log(arr[1]);
+// const answer = +prompt("Вам есть 18?", "");
+// console.log(answer + 10);
+const answers = [];
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Сколько вам лет?", "");
+console.log(answers);
